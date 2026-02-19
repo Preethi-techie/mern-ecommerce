f7f509f73372fc3ad57806ba9d4efb57f2c5be54
Preethi Sekar <preethisekar253@gmail.com>
2026-02-19 16:45:59 +0530

    Project completed

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.Mongo_URI);
-        console.log(`Mongodb connected : ${conn.connection.host}`);
-     } catch(error) {
-        console.log("Error connecting to MONGODB", error.message);
-        process.exit(1);
-        
-    }
-}
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+    process.exit(1);
+  }
+};
